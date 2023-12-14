256cd2cc791994cd825eeb3c473f24821c5c98f5
Nathan Godoy <godmnathan.code@gmail.com>
2023-12-14 12:56:00 -0300

    Add files via upload

---
+++ Introduction/Java Output Formatting/Solution.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("================================");
+
+        while (scanner.hasNext()) {
+            String str = scanner.next();
+            int num = scanner.nextInt();
+
+            System.out.printf("%-15s%03d%n", str, num);
+        }
+
+        System.out.println("================================");
+
+        scanner.close();
+    }
+}
